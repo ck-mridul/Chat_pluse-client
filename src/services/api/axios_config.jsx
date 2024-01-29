cd8da87975c03e7e9df2d155eddb574c6209151e
ckmridul <ckmkanad16@gmail.com>
2024-01-29 12:08:14 +0530

    add ssl

--- src/services/api/axios_config.jsx
+++ src/services/api/axios_config.jsx
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-export const baseURL = 'http://13.211.162.117';
-export const wsURL = 'ws://13.211.162.117';
+export const baseURL = 'https://13.211.162.117:8000';
+export const baseURL1 = 'https://13.211.162.117';
+export const wsURL = 'wss://13.211.162.117';
 
 
 export const axiosInstance = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL1}/api`,
 });
 
 const axiosAuth = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL1}/api`,
 });
 
 axiosAuth.interceptors.request.use(
